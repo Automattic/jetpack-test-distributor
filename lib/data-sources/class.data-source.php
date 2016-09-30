1706b98912fc310b6fc53802881f13a7dc567ce7
Jacob Morrison <jomorrison@gmail.com>
2016-09-30 16:09:38 -0700

    initial Jetpack_Data_Source class

--- lib/data-sources/class.data-source.php
+++ lib/data-sources/class.data-source.php
@@ -6,6 +6,17 @@ namespace Automattic\Human_Testable\Data_Sources;
  */
 abstract class Data_Source {
 	/**
+	 * Prepares a test item object with an array of attributes from the database
+	 *
+	 * @param  array $attributes List of attributes to use in new object
+	 * @return Automattic\Human_Testable\Test_Items\Test_Item
+	 */
+	protected function prepare( $attributes ) {
+			$className = $this->get_test_item_class();
+			return new $className( $attributes );
+	}
+
+	/**
 	 * Returns all available tests
 	 *
 	 * @return array All tests
@@ -18,14 +29,21 @@ abstract class Data_Source {
 	 * @param string $site_id ID of the site requesting tests.
 	 * @return array Array of test IDs
 	 */
-	abstract public function get_sent_tests( $site_id );
+	abstract public function get_completed_tests( $site_id );
 
 	/**
-	 * Marks tests as being sent to a site ID
+	 * Marks a test as being completed or skipped for a site ID
 	 *
-	 * @param string $site_id ID of the site requesting tests.
-	 * @param array  $test_ids Array of all test IDs sent to the site.
+	 * @param int $site_id ID of the site requesting tests.
+	 * @param int $test_id ID of the test that was completed or skipped
 	 * @return boolean Success result.
 	 */
-	abstract public function save_sent_tests( $site_id, $test_ids );
+	abstract public function save_completed_test( $site_id, $test_id, $skipped = false );
+
+	/**
+	 * Returns the test item class to use for this data source
+	 *
+	 * @return string Class name
+	 */
+	abstract public function get_test_item_class();
 }
---
+++ lib/data-sources/class.pdo-jetpack-data-source.php
@@ -0,0 +1,76 @@
+<?php
+namespace Automattic\Human_Testable\Data_Sources;
+
+require_once( __DIR__ . DIRECTORY_SEPARATOR . 'class.data-source.php' );
+require_once( dirname( __DIR__ ) . DIRECTORY_SEPARATOR . 'test-items' . DIRECTORY_SEPARATOR . 'class.jetpack-test-item.php' );
+
+use PDO;
+use Automattic\Human_Testable\Test_Items\Jetpack_Test_Item;
+
+/**
+ * Abstract class for a data source
+ */
+class PDO_Jetpack_Data_Source extends Data_Source {
+	/**
+	 * PDO connection
+	 *
+	 * @var \PDO
+	 */
+	private $pdo;
+
+	/**
+	 * Constructor
+	 *
+	 * @param string $dsn      DSN string for DBO connection.
+	 * @param string $username Username for the connection.
+	 * @param string $password Password for the connection.
+	 * @param array  $options DBO connection options.
+	 */
+	public function __construct( $dsn, $username, $password, $options = [] ) {
+		$this->pdo = new PDO( $dsn, $username, $password, $options );
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function get_tests() {
+		$jti_query = $this->pdo->prepare( 'SELECT `jti`.* FROM `jetpack_test_items` `jti` WHERE `jti`.`active`=1' );
+		$jti_query->execute();
+		$tests = array();
+		while ( $row = $jti_query->fetch( PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT ) ) {
+			$test_item = $this->prepare( $row );
+			$tests[ $test_item->get_id() ] = $test_item;
+		}
+		return $tests;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function get_completed_tests( $site_id ) {
+		$jtic_query = $this->pdo->prepare( 'SELECT `jtic`.`jetpack_test_item_id` FROM `jetpack_test_items_completed` `jtic` WHERE `jtic`.`site_id`=:site_id' );
+		$jtic_query->execute( array( ':site_id' => $site_id ) );
+		return $jtic_query->fetchColumn();
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function save_completed_test( $site_id, $test_id, $skipped = false ) {
+		if ( true === $skipped ) {
+			$skipped = 1;
+		} else {
+			$skipped = 0;
+		}
+		$insert_sql = 'INSERT INTO `jetpack_test_items_completed` SET `site_id`=:site_id, `jetpack_test_item_id`=:jetpack_test_item_id, `skipped`=:skipped';
+		$jtic_insert_query = $this->pdo->prepare( $insert_sql );
+		return $jtic_insert_query->execute( array( ':site_id' => $site_id, ':jetpack_test_item_id' => $test_id, ':skipped' => $skipped ) );
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function get_test_item_class() {
+		return Jetpack_Test_Item::class;
+	}
+}
