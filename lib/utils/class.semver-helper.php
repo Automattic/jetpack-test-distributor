0dffbaac7c49af4516e48c1f51e3873f036f2cbd
Jacob Morrison <jomorrison@gmail.com>
2016-10-01 10:49:45 -0700

    Expand version to full server and implement major test
    PHP’s `version_compare()` doesn’t seem to think 7.0. === 7.0.0 so I’ve
    expanded it. Also simplifies `Semver_Helper::is_major_release()`
    implementation.

--- lib/utils/class.semver-helper.php
+++ lib/utils/class.semver-helper.php
@@ -17,7 +17,6 @@ class Semver_Helper {
 		$test_version = static::normalize_version( $test_version );
 		$min_version = static::normalize_version( $min_version );
 		$max_version = static::normalize_version( $max_version );
-
 		if ( ! isset( $test_version ) ) {
 			if ( isset( $max_version ) ) {
 				return false;
@@ -36,24 +35,51 @@ class Semver_Helper {
 	/**
 	 * Normalize a version. Nulls invalid version numbers.
 	 *
-	 * @param  string $version Version to normalize.
+	 * @param  string $version       Version to normalize.
+	 * @param  bool   $return_number Returns just the number and not branch/beta tags.
 	 * @return string          Normalized version.
 	 */
-	static public function normalize_version( $version ) {
-		if ( isset( $version ) &&  preg_match( '/^\d+\.\d+/', $version ) !== 1 ) {
+	static public function normalize_version( $version, $return_number = false ) {
+		if ( ! isset( $version ) || preg_match( '/^\d+\.\d+/', $version ) !== 1 ) {
 			return null;
 		}
-		return $version;
+		$version_parts = explode( '-', $version );
+		$version_parts[0] = static::expand_version_number( $version_parts[0] );
+		if ( $return_number ) {
+			return $version_parts[0];
+		}
+		return implode( '-', $version_parts );
+	}
+
+	/**
+	 * Expands version numbers to be [major].[minor].[patch]
+	 * For example:
+	 *   1.0 -> 1.0.0
+	 *   3 -> 3.0.0
+	 *
+	 * @param  string $version Version number.
+	 * @return string
+	 */
+	static public function expand_version_number( $version ) {
+		$version_parts = explode( '.', $version );
+		if ( ! isset( $version_parts[1] ) ) {
+			$version_parts[1] = 0;
+		}
+		if ( ! isset( $version_parts[2] ) ) {
+			$version_parts[2] = 0;
+		}
+		return implode( '.', $version_parts );
 	}
 
 	/**
 	 * Tests is a version is a major version release
 	 *
-	 * @param  string  $version Version to test.
-	 * @return boolean          Result of the test.
+	 * @param  string $version Version to test.
+	 * @return boolean         Result of the test.
 	 * @todo
 	 */
 	static public function is_major_release( $version ) {
-		return true;
+		$version = static::normalize_version( $version );
+		return preg_match( '/^\d+\.0.0$/', $version ) === 1;
 	}
 }
