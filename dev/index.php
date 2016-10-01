14679b8bd2c40801f2d1ce28ebc9b469ea739ac4
Jacob Morrison <jomorrison@gmail.com>
2016-09-30 19:47:58 -0700

    Starting  for retrieving tests for the specific site and environment

--- dev/index.php
+++ dev/index.php
@@ -1,47 +1,56 @@
 <?php
 require_once( dirname( __DIR__ ) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php' );
+require_once( dirname( __DIR__ ) . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'class.test-distributor.php' );
+require_once( dirname( __DIR__ ) . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'data-sources' . DIRECTORY_SEPARATOR . 'class.pdo-jetpack-data-source.php' );
 require_once( __DIR__ . DIRECTORY_SEPARATOR . 'lists' . DIRECTORY_SEPARATOR . 'browsers.php' );
 require_once( __DIR__ . DIRECTORY_SEPARATOR . 'lists' . DIRECTORY_SEPARATOR . 'hosts.php' );
 require_once( __DIR__ . DIRECTORY_SEPARATOR . 'lists' . DIRECTORY_SEPARATOR . 'importances.php' );
 require_once( __DIR__ . DIRECTORY_SEPARATOR . 'lists' . DIRECTORY_SEPARATOR . 'modules.php' );
-( new josegonzalez\Dotenv\Loader( dirname( __DIR__ ) . DIRECTORY_SEPARATOR . '.env' ) )->parse()->toEnv();
+
+use Automattic\Human_Testable\Data_Sources\PDO_Jetpack_Data_Source;
+use Automattic\Human_Testable\Test_Distributor;
+
+( new josegonzalez\Dotenv\Loader( dirname( __DIR__ ) . DIRECTORY_SEPARATOR . '.env' ) )->parse()->define();
+
+$jetpack_data_source = new PDO_Jetpack_Data_Source( 'mysql:host=' . DB_HOST . ';dbname=' . DB_DATABASE, DB_USERNAME, DB_PASSWORD );
+$test_distributor = new Test_Distributor( $jetpack_data_source );
 
 function draw_select_list( $list, $name, $multiple = false ) {
-  $value = '';
-  $multiple_str = '';
-  if ( $multiple ) {
-    $multiple_str = ' multiple="multiple"';
-  }
-  if ( isset( $_POST[ $name ] ) ) {
-    $value = $_POST[ $name ];
-  }
-  echo '<select name="'. $name .'">';
-  echo '<option value=""></option>';
-  foreach ( $list as $key => $label ) {
-    $selected_str = '';
-    if ( $value === $key ) {
-      $selected_str = ' selected="selected"';
-    }
-    echo '<option value="'. $key .'"'. $selected_str .'>'. $label .'</option>';
-  }
-  echo '</select>';
+	$value = '';
+	$multiple_str = '';
+	if ( $multiple ) {
+		$multiple_str = ' multiple="multiple"';
+	}
+	if ( isset( $_POST[ $name ] ) ) {
+		$value = $_POST[ $name ];
+	}
+	echo '<select name="'. $name .'">';
+	echo '<option value=""></option>';
+	foreach ( $list as $key => $label ) {
+		$selected_str = '';
+		if ( $value === $key ) {
+			$selected_str = ' selected="selected"';
+		}
+		echo '<option value="'. $key .'"'. $selected_str .'>'. $label .'</option>';
+	}
+	echo '</select>';
 }
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US">
 <head profile="http://gmpg.org/xfn/11">
-  <meta charset="utf-8" />
-  <title>Test Distributor</title>
+	<meta charset="utf-8" />
+	<title>Test Distributor</title>
 </head>
 <body>
-  <form method="post">
-    <div><label>Site ID</label> <input name="site_id" value="<?php echo isset( $_POST['site_id'] ) ? $_POST['site_id'] : '1'; ?>" /></div>
-    <div><label>Version</label> <input name="version" value="<?php echo isset( $_POST['version'] ) ? $_POST['version'] : ''; ?>" /></div>
-    <div><label>Host</label> <?php echo draw_select_list( $hosts, 'host' ); ?></div>
-    <div><label>Browser</label> <?php echo draw_select_list( $browsers, 'browser' ); ?></div>
-    <!-- <div><label>Module</label> <?php echo draw_select_list( $hosts ); ?></div> -->
-    <input type="submit" name="submit" value="Submit" />
-  </form>
+	<form method="post">
+		<div><label>Site ID</label> <input name="site_id" value="<?php echo isset( $_POST['site_id'] ) ? $_POST['site_id'] : '1'; ?>" /></div>
+		<div><label>Version</label> <input name="version" value="<?php echo isset( $_POST['version'] ) ? $_POST['version'] : ''; ?>" /></div>
+		<div><label>Host</label> <?php echo draw_select_list( $hosts, 'host' ); ?></div>
+		<div><label>Browser</label> <?php echo draw_select_list( $browsers, 'browser' ); ?></div>
+		<!-- <div><label>Module</label> <?php echo draw_select_list( $hosts ); ?></div> -->
+		<input type="submit" name="submit" value="Submit" />
+	</form>
 <?php
 
 ?>
--- lib/class.test-distributor.php
+++ lib/class.test-distributor.php
@@ -1,5 +1,14 @@
 <?php
 namespace Automattic\Human_Testable;
 
+require_once( __DIR__ . DIRECTORY_SEPARATOR . 'data-sources' . DIRECTORY_SEPARATOR . 'class.data-source.php' );
+
+use Automattic\Human_Testable\Data_Sources\Data_Source;
+
 class Test_Distributor {
+	private $data_source;
+
+	public function __construct( Data_Source $data_source ) {
+		$this->data_source = $data_source;
+	}
 }
